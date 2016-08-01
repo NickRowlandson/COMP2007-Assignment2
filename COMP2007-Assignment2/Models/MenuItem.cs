4a26a9a38ceb0e4f9bd5d02899c349e0301eda9e
NickRowlandson <nicholasrowlandson@gmail.com>
2016-08-01 13:38:10 -0400

    Setup database and MenuItemsController.

---
+++ COMP2007-Assignment2/Models/MenuItem.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace COMP2007_Assignment2.Models
+{
+    [Table("menuitems")]
+    public class MenuItem
+    {
+        /// <summary>
+        /// This is the empty constructor
+        /// </summary>
+        public MenuItem()
+        {
+
+        }
+
+        /// <summary>
+        /// This constructor takes one parameter - Title
+        /// </summary>
+        /// <param name="Title"></param>
+        public MenuItem(string Title)
+        {
+            this.Title = Title;
+        }
+
+        [Key]
+        public virtual int ID { get; set; }
+        public virtual string Title { get; set; }
+        public virtual decimal Price { get; set; }
+        public virtual string Description { get; set; }
+        public virtual string ItemThumbnail { get; set; }
+        public virtual string ItemImage { get; set; }
+    }
+}
